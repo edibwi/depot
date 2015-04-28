aa6b3d6b0a0e06e4e7bb3043317d82ed10aa5923
edibwi <its.edi@gmail.com>
2015-04-28 10:19:09 +0800

    playtime chapter 8

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'dt','Programming Ruby 1.9'
+    assert_select 'tr', 3
+    assert_select 'h1','Listing Products'
   end
 
   test "should get new" do
